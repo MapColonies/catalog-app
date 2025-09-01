2242b95143b5c3e9050cc390b20a11f464b26851
Eliav Maimon <liavma3@gmail.com>
2025-09-01 14:10:18 +0300

    fix: patch control state when re-select shapefile

--- src/discrete-layer/components/layer-details/raster/layer-datails-form.raster.tsx
+++ src/discrete-layer/components/layer-details/raster/layer-datails-form.raster.tsx
@@ -1119,6 +1119,11 @@ export const InnerRasterForm = (
                 
                 importShapeFileFromClient((ev, fileType, fileName) => {
                   const shpFile = (ev.target?.result as unknown) as ArrayBuffer;
+
+                  setPPCheckPerformed(false);
+                  setPPFeatures([]);
+                  setShowCurtain(true);
+
                   void proccessShapeFile(shpFile, fileType, targetName, fileName)
                     .then((parsedPPData) => {
                       
@@ -1262,6 +1267,7 @@ export const InnerRasterForm = (
                   <Button
                     outlined
                     type="button"
+                    disabled={ isValidatingSource }
                     onClick={(): void => {
                       setLoadingPolygonParts(false);
                       setPPCheckPerformed(false);
