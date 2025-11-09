64116bcc9575f62d396cb369673743ab246fd2b7
Eliav Maimon <liavma3@gmail.com>
2025-11-09 16:42:52 +0200

    fix: add comment

--- src/common/models/raster-ingestion-files-structure.ts
+++ src/common/models/raster-ingestion-files-structure.ts
@@ -1,6 +1,8 @@
 // RASTER GENERAL CONFIGURATION
 import { RasterIngestionFilesTypeConfig } from "../../discrete-layer/models";
 
+// FIELD selectablePattern can receive letters, and may optionally end with a '*' character,
+// which acts as a wildcard to match any sequence of characters following the prefix in the file name.
 interface IRasterFileGroupConfig {
   allowedExt: string[];
   selectableExt: string[];
