91c6eb43355e5f785e0f5896efcb13c94f116c0e
alebinson <newdarkvirus@gmail.com>
2025-11-11 13:32:46 +0200

    fix: curtain with loading component

---
+++ src/discrete-layer/components/layer-details/raster/curtain/curtain.component.tsx
@@ -0,0 +1,18 @@
+import { CircularProgress } from "@map-colonies/react-core";
+import { Box } from '@map-colonies/react-components';
+
+import './curtain.component.css';
+
+interface CurtainProps {
+  showProgress?: boolean;
+}
+
+export const Curtain: React.FC<CurtainProps> = ({showProgress}) => {
+  return (
+    <Box className={'curtain'}>
+      {
+        showProgress && <CircularProgress className={'progress'} size='xlarge' />
+      }
+    </Box>
+  );
+};
