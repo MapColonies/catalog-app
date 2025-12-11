25f32d8f58ba5ccc60b90a8280720e148d5d6b45
Eliav Maimon <liavma3@gmail.com>
2025-12-11 13:21:13 +0200

    fix: clean

--- src/discrete-layer/components/system-status/cell-renderer/job-details.raster-job-data.tsx
+++ src/discrete-layer/components/system-status/cell-renderer/job-details.raster-job-data.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
 import { get } from 'lodash';
 import { ICellRendererParams } from 'ag-grid-community';
@@ -9,9 +9,9 @@ import { Hyperlink } from '../../../../common/components/hyperlink/hyperlink';
 import { RasterIngestionJobType } from '../../../../common/models/raster-job';
 import { DETAILS_ROW_ID_SUFFIX } from '../../../../common/components/grid';
 import { JobModelType, TaskModelType, useStore } from '../../../models';
+import useZoomLevelsTable from '../../export-layer/hooks/useZoomLevelsTable';
 import { CopyButton } from '../../job-manager/job-details.copy-button';
 import { ErrorsCountPresentor, ErrorsSummary, getErrorCount } from '../../helpers/jobUtils';
-import useZoomLevelsTable from '../../export-layer/hooks/useZoomLevelsTable';
 
 import './info-area.css';
 import './job-details.raster-job-data.css';
@@ -49,7 +49,7 @@ const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data 
     return count;
   };
 
-  const fetchTask = async () => {
+  const fetchTask = useCallback(async () => {
     try {
       const result = await store.queryFindTasks({
         params: {
@@ -74,9 +74,9 @@ const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data 
         }
       }
     } catch { }
-  }
+  }, [store, jobData]);
 
-  const computeZoomLevel = () => {
+  const computeZoomLevel = useCallback(() => {
     const ingestionResolution = get(jobData?.parameters, 'ingestionResolution')?.toString();
     if (!ingestionResolution) {
       return;
@@ -89,13 +89,13 @@ const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data 
     if (index >= 0) {
       setZoomLevel(index);
     }
-  }
+  }, [jobData]);
 
   const getGpkgFilesPath = () => {
     return jobData.parameters?.inputFiles?.gpkgFilesPath?.[0];
   }
 
-  const isNewFormatJob = () => {
+  const hasGpkgPath = () => {
     return !!getGpkgFilesPath();
   }
 
@@ -117,7 +117,8 @@ const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data 
   const errorsMessage = intl.formatMessage({ id: 'general.errors.text' });
 
   const zoomLabel = zoomLevel !== undefined ? `(${zoomLevel})` : '';
-  const rasterInfo = (jobData.parameters && isNewFormatJob()) ?
+
+  const rasterInfo = (jobData.parameters && hasGpkgPath()) ?
     `${getGpkgFilesPath()} ${zoomLabel}` :
     intl.formatMessage({ id: 'general.deprecated-job.text' });
 
@@ -129,14 +130,12 @@ const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data 
 
   return (
     <Box id='rasterJobData' className='jobDataContainer'>
-      {
-        <AutoDirectionBox>
-          {
-            !isLoading ? rasterInfo :
-              <CircularProgress size='xsmall'></CircularProgress>
-          }
-        </AutoDirectionBox>
-      }
+      <AutoDirectionBox>
+        {
+          !isLoading ? rasterInfo :
+            <CircularProgress size='xsmall'></CircularProgress>
+        }
+      </AutoDirectionBox>
       {
         !isLoading &&
         <Box className={`linkItem errorsCountContainer ${errorsCount === 0 ? 'noErrors' : ''}`} key={`${jobData.id}`}>
@@ -171,7 +170,7 @@ const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data 
               <CopyButton text={task?.parameters?.TBDlinkTBD ?? ''} key={'errorsReportLink'} />
             </>
           }
-          {!hasErrors && isNewFormatJob() && task &&
+          {!hasErrors && hasGpkgPath() && task &&
             intl.formatMessage({ id: 'general.no-errors.text' })
           }
         </Box>
