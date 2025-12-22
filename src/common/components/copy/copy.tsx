7550081e2e0e8236995277425d26b28b8446c535
ellamartirosyan <ellamartirosyan@gmail.com>
2025-12-22 20:02:45 +0200

    fix: eslint

--- src/common/components/copy/copy.tsx
+++ src/common/components/copy/copy.tsx
@@ -2,7 +2,6 @@ import React, { ReactElement, useEffect, useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { IconButton } from '@map-colonies/react-core';
 import { Box } from '@material-ui/core';
-import CONFIG from '../../../common/config';
 
 import './style.css';
 
@@ -16,29 +15,32 @@ export const Copy = (props: IProps) => {
   const [isCopied, setIsCopied] = useState(false);
 
   useEffect(() => {
-    isCopied && setTimeout(setIsCopied, 1500, false)
-  }, [isCopied])
+    isCopied && setTimeout(setIsCopied, 1500, false);
+  }, [isCopied]);
 
   return (
     <>
       <CopyToClipboard text={props.value} onCopy={(): void => setIsCopied(true)}>
-        <Box className={`icon-wrapper`}>
-          {props.copyToClipboardChildren ?
-            props.copyToClipboardChildren :
+        <Box className="icon-wrapper">
+          {
+            props.copyToClipboardChildren
+            ? props.copyToClipboardChildren
+            : <IconButton
+                type="button"
+                className={"mc-icon-Copy"}
+                onChange={(): void => {
+                  setIsCopied(true);
+                }}
+                label={isCopied ? 'Success' : 'Copy'}
+              />
+          }
+          {
+            isCopied &&
             <IconButton
-              type="button"
-              className={"mc-icon-Copy"}
-              onChange={(): void => {
-                setIsCopied(true)
-              }}
-              label={isCopied ? 'Success' : 'Copy'}
-            />}
-
-          {isCopied && <IconButton
-            className={`mc-icon-Ok`}
-            style={{ color: "var(--mdc-theme-gc-success)" }}
-          />}
-
+              className="mc-icon-Ok"
+              style={{ color: "var(--mdc-theme-gc-success)" }}
+            />
+          }
         </Box>
       </CopyToClipboard>
     </>
--- src/discrete-layer/components/system-status/system-core-info/external-services/external-services.tsx
+++ src/discrete-layer/components/system-status/system-core-info/external-services/external-services.tsx
@@ -1,53 +1,51 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 import { Box } from '@map-colonies/react-components';
 import {
   CollapsibleList,
   SimpleListItem,
   Typography,
 } from '@map-colonies/react-core';
-import { FormattedMessage, useIntl } from 'react-intl';
 import { Copy } from '../../../../../common/components/copy';
 import { CategorizedServices } from '../system-core-info.dialog';
+
 import './external-services.css';
 
 interface ExternalServicesProps {
   services: CategorizedServices;
 }
 
-export const ExternalServices: React.FC<ExternalServicesProps> = ({
-  services,
-}: ExternalServicesProps) => {
-  const intl = useIntl();
-
+export const ExternalServices: React.FC<ExternalServicesProps> = ({ services }: ExternalServicesProps) => {
   return (
     <Box className="listsContainer">
-      {Object.entries(services).map(([category, services]) => {
-        return (
-          <CollapsibleList
-            handle={
-              <SimpleListItem
-                text={category}
-                metaIcon="chevron_right"
-              />
-            }
-          >
-            {(services ?? []).map(({ url, display }) => {
-              return (
-                <Box className="externalService">
-                  <Typography className="displayName" tag="p">
-                    <FormattedMessage id={display} />
-                  </Typography>
-
-                  <Typography className="urlText" tag="p">
-                    {`${url as string}`}
-                  </Typography>
-                  <Copy value={url as string}/>
-                </Box>
-              );
-            })}
-          </CollapsibleList>
-        );
-      })}
+      {
+        Object.entries(services).map(([category, services]) => {
+          return (
+            <CollapsibleList
+              handle={
+                <SimpleListItem
+                  text={category}
+                  metaIcon="chevron_right"
+                />
+              }
+            >
+              {(services ?? []).map(({ url, display }) => {
+                return (
+                  <Box className="externalService">
+                    <Typography className="displayName" tag="p">
+                      <FormattedMessage id={display} />
+                    </Typography>
+                    <Typography className="urlText" tag="p">
+                      {`${url as string}`}
+                    </Typography>
+                    <Copy value={url as string}/>
+                  </Box>
+                );
+              })}
+            </CollapsibleList>
+          );
+        })
+      }
     </Box>
   );
 };
