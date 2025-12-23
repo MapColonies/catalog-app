ba90d614bc16b8e02da8a28cd3cffa08daa94c46
ellamartirosyan <ellamartirosyan@gmail.com>
2025-12-23 14:26:26 +0200

    fix: eslint

--- src/discrete-layer/components/job-manager/cell-renderer/job-details/job-details.fail-reason-area.tsx
+++ src/discrete-layer/components/job-manager/cell-renderer/job-details/job-details.fail-reason-area.tsx
@@ -1,5 +1,4 @@
 import React,{useState, useEffect} from 'react';
-import { useIntl } from 'react-intl';
 import { Typography } from '@map-colonies/react-core';
 import { Box } from '@map-colonies/react-components';
 import { Copy } from '../../../../../common/components/copy/copy';
@@ -12,19 +11,12 @@ interface FailReasonAreaProps {
   key?: string;
 }
 
-export const FailReasonArea: React.FC<FailReasonAreaProps> = ({
-  failReason,
-  show,
-  key = '',
-}) => {
-  const intl = useIntl();
-
+export const FailReasonArea: React.FC<FailReasonAreaProps> = ({ failReason, show, key = '' }) => {
   const [containerClass, setContainerClass] = useState('failReasonAreaContainer');
 
   useEffect(() => {
     setContainerClass(`failReasonAreaContainer ${show ? 'show' : ''}`);
-  }, [show])
-
+  }, [show]);
 
   return (
     <td colSpan={5}>
@@ -32,7 +24,6 @@ export const FailReasonArea: React.FC<FailReasonAreaProps> = ({
         <Typography className="failReasonText" tag="p">
           {failReason}
         </Typography>
-
         <Copy value={failReason}/>
       </Box>
     </td>
