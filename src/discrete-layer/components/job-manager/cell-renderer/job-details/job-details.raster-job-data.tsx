28873e6e240a0c8e619b2e0b45ba707d2894d1cd
Ella Martirosyan <34953077+EllaMartirosyan@users.noreply.github.com>
2026-01-05 19:10:54 +0200

    fix: job manager errors summary link and status icons (#823)

--- src/discrete-layer/components/job-manager/cell-renderer/job-details/job-details.raster-job-data.tsx
+++ src/discrete-layer/components/job-manager/cell-renderer/job-details/job-details.raster-job-data.tsx
@@ -1,16 +1,16 @@
+import { ICellRendererParams } from 'ag-grid-community';
+import { get } from 'lodash';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
-import { get } from 'lodash';
-import { ICellRendererParams } from 'ag-grid-community';
 import { Box } from '@map-colonies/react-components';
 import { CircularProgress, IconButton, Tooltip, Typography, useTheme } from '@map-colonies/react-core';
-import { Copy } from '../../../../../common/components/copy/copy';
 import { AutoDirectionBox } from '../../../../../common/components/auto-direction-box/auto-direction-box.component';
-import { Hyperlink } from '../../../../../common/components/hyperlink/hyperlink';
-import { RasterIngestionJobType } from '../../../../../common/models/raster-job';
+import { Copy } from '../../../../../common/components/copy/copy';
 import { DETAILS_ROW_ID_SUFFIX } from '../../../../../common/components/grid';
+import { Hyperlink } from '../../../../../common/components/hyperlink/hyperlink';
 import { Domain } from '../../../../../common/models/domain';
 import { RasterErrorsSummary } from '../../../../../common/models/job-errors-summary.raster';
+import { RasterIngestionJobType } from '../../../../../common/models/raster-job';
 import { JobModelType, Status, TaskModelType, useStore } from '../../../../models';
 import useZoomLevelsTable from '../../../export-layer/hooks/useZoomLevelsTable';
 import { getRasterErrorCount, JobErrorsSummary } from '../../../job-errors-summary/job-errors-summary';
@@ -22,14 +22,12 @@ interface JobDetailsRasterJobDataProps extends ICellRendererParams { }
 
 const MAX_ERRORS_SHOWN = 99;
 
-const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data }) => {
+export const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data }) => {
+  const jobData = data as JobModelType;
   const store = useStore();
   const intl = useIntl();
   const theme = useTheme();
   const ZOOM_LEVELS_TABLE = useZoomLevelsTable();
-
-  const jobData = data as JobModelType;
-
   const [task, setTask] = useState<TaskModelType>();
   const [errorsCount, setErrorsCount] = useState(0);
   const [zoomLevel, setZoomLevel] = useState<number | undefined>(undefined);
@@ -42,14 +40,12 @@ const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data 
 
   const calculateErrorsCount = (errors: RasterErrorsSummary): number => {
     let count = 0;
-
     Object.keys(errors.errorsCount).forEach((key) => {
       const errorCount = getRasterErrorCount(errors, key);
       if (errorCount.exceeded === true || typeof errorCount.exceeded === 'undefined') {
         count += errorCount.count ?? 0;
       }
     });
-
     return count;
   };
 
@@ -61,18 +57,13 @@ const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data 
           type: 'validation'
         }
       });
-
       if (!result?.findTasks[0]) {
         return undefined;
       }
-
       const task = { ...result.findTasks[0] };
-
       if (task) {
         setTask(task);
-
         const errorsCount = task?.parameters?.errorsSummary;
-
         if (errorsCount) {
           setErrorsCount(calculateErrorsCount(errorsCount));
         }
@@ -85,11 +76,9 @@ const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data 
     if (!ingestionResolution) {
       return;
     }
-
     const index = Object.values(ZOOM_LEVELS_TABLE)
       .map((value) => value.toString())
       .findIndex(value => value === ingestionResolution);
-
     if (index >= 0) {
       setZoomLevel(index);
     }
@@ -97,19 +86,19 @@ const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data 
 
   const getGpkgFilesPath = () => {
     return jobData.parameters?.inputFiles?.gpkgFilesPath?.[0];
-  }
+  };
 
   const hasGpkgPath = () => {
     return !!getGpkgFilesPath();
-  }
+  };
 
   const errorsMessage = intl.formatMessage({ id: 'general.errors.text' });
 
   const zoomLabel = zoomLevel !== undefined ? `(${zoomLevel})` : '';
 
-  const rasterInfo = (jobData.parameters && hasGpkgPath()) ?
-    `${getGpkgFilesPath()} ${zoomLabel}` :
-    intl.formatMessage({ id: 'general.deprecated-job.text' });
+  const rasterInfo = (jobData.parameters && hasGpkgPath())
+    ? `${getGpkgFilesPath()} ${zoomLabel}`
+    : intl.formatMessage({ id: 'general.deprecated-job.text' });
 
   const hasErrors = errorsCount > 0;
   const isTaskFailed = task?.status === Status.Failed;
@@ -118,57 +107,56 @@ const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data 
     if (!isRasterJob) {
       return;
     }
-
     fetchTask();
     computeZoomLevel();
   }, [jobData, jobData.parameters, isRasterJob]);
 
   useEffect(() => {
     const loading = !jobData.parameters;
-
     setIsLoading(loading);
   }, [jobData]);
-
 
   if (!isRasterJob) {
     return null;
   }
 
   return (
-    <Box id='rasterJobData' className='jobDataContainer'>
-      <AutoDirectionBox>
-        {
-          !isLoading ? rasterInfo : <CircularProgress size='xsmall'></CircularProgress>
-        }
-      </AutoDirectionBox>
+    <Box id="rasterJobData" className="jobDataContainer">
+      <Box className="content">
+        <AutoDirectionBox>
+          {
+            !isLoading
+              ? rasterInfo
+              : <CircularProgress size="xsmall"></CircularProgress>
+          }
+        </AutoDirectionBox>
+      </Box>
       {
         !isLoading &&
-        <Box className='errorsContainerPosition' key={`${jobData.id}`}>
+        <Box className="errorsContainerPosition" key={`${jobData.id}`}>
           {
             !isTaskFailed && hasErrors &&
-            <Box className='errorsSummaryContainer'>
+            <Box className="errorsSummaryContainer">
               <IconButton
-                className='mc-icon-Status-Warnings error'
+                className="mc-icon-Status-Warnings error statusIcon"
                 onClick={(e): void => {
                   e.preventDefault();
                   e.stopPropagation();
                 }}
               />
-
               <Tooltip content={
                 <Box>
                   {JobErrorsSummary(theme, task?.parameters?.errorsSummary, "reportItem")}
                 </Box>
               }>
-                <Hyperlink className='error' url={task?.parameters?.report?.url ?? ''} label={`${errorsCount > MAX_ERRORS_SHOWN ? `+${MAX_ERRORS_SHOWN}` : errorsCount} ${errorsMessage}`} />
+                <Hyperlink className="error" url={task?.parameters?.report?.url ?? ''} label={`${errorsCount > MAX_ERRORS_SHOWN ? `+${MAX_ERRORS_SHOWN}` : errorsCount} ${errorsMessage}`} />
               </Tooltip>
-
               <Copy value={task?.parameters?.report?.url ?? ''} iconStyle={{ fontSize: `20px` }} key={'errorsReportLink'} />
             </Box>
           }
           {
             !isTaskFailed && !hasErrors && hasGpkgPath() && task &&
-            <Box className='success'>
+            <Box className="success">
               {intl.formatMessage({ id: 'general.no-errors.text' })}
             </Box>
           }
@@ -188,6 +176,4 @@ const JobDetailsRasterJobData: React.FC<JobDetailsRasterJobDataProps> = ({ data 
       }
     </Box>
   );
-}
-
-export default JobDetailsRasterJobData;
+};
--- src/discrete-layer/components/job-manager/cell-renderer/job-details/job.details.export-job-data.tsx
+++ src/discrete-layer/components/job-manager/cell-renderer/job-details/job.details.export-job-data.tsx
@@ -1,7 +1,7 @@
+import { ICellRendererParams } from 'ag-grid-community';
+import { get } from 'lodash';
 import React from 'react';
 import { useIntl } from 'react-intl';
-import { get } from 'lodash';
-import { ICellRendererParams } from 'ag-grid-community';
 import { Box } from '@map-colonies/react-components';
 import { Typography } from '@map-colonies/react-core';
 import { Copy } from '../../../../../common/components/copy/copy';
@@ -16,72 +16,70 @@ interface JobDetailsExportJobDataProps extends ICellRendererParams {}
 
 const NONE = 0;
 
-const JobDetailsExportJobData: React.FC<JobDetailsExportJobDataProps> = ({ data }) => {
-    const intl = useIntl();
-    const jobData = data as JobModelType;
-    
-    const isExportJob = jobData.type?.toLowerCase().includes('export');
+export const JobDetailsExportJobData: React.FC<JobDetailsExportJobDataProps> = ({ data }) => {
+  const intl = useIntl();
+  const jobData = data as JobModelType;
+  
+  const isExportJob = jobData.type?.toLowerCase().includes('export');
 
-    const jobDescription = jobData.description as string | undefined ?? '';
-    const exportLinks = get(jobData, `parameters.callbackParams.links`) as Record<string, string> | undefined;
+  const jobDescription = jobData.description as string | undefined ?? '';
+  const exportLinks = get(jobData, `parameters.callbackParams.links`) as Record<string, string> | undefined;
 
-    const expirationTimeUTC: string = get(jobData, `parameters.cleanupData.cleanupExpirationTimeUTC`);
-    let formattedExpirationTime = '';
-    let hasExpired: boolean = false;
+  const expirationTimeUTC: string = get(jobData, `parameters.cleanupData.cleanupExpirationTimeUTC`);
+  let formattedExpirationTime = '';
+  let hasExpired: boolean = false;
 
-    if (expirationTimeUTC) {
-      const expirationTime = new Date(expirationTimeUTC);
-      formattedExpirationTime = dateFormatter(expirationTime);
-      const today = new Date();
-      hasExpired = expirationTime < today;
-    }
+  if (expirationTimeUTC) {
+    const expirationTime = new Date(expirationTimeUTC);
+    formattedExpirationTime = dateFormatter(expirationTime);
+    const today = new Date();
+    hasExpired = expirationTime < today;
+  }
 
-    const jobStatus = jobData.status;
+  const jobStatus = jobData.status;
 
-    // const exportLinkLabel = intl.formatMessage({id: 'system-status.export-details.link.label'});
-    const jobNoDescriptionText = intl.formatMessage({id: 'system-status.export-details.no-description.text'});
-    
-    const jobDescriptionText = intl.formatMessage({ id: 'system-status.export-details.description.label' } );
+  // const exportLinkLabel = intl.formatMessage({id: 'system-status.export-details.link.label'});
+  const jobNoDescriptionText = intl.formatMessage({id: 'system-status.export-details.no-description.text'});
+  
+  const jobDescriptionText = intl.formatMessage({ id: 'system-status.export-details.description.label' } );
 
-    if (!(isExportJob as boolean)) return null;
+  if (!(isExportJob as boolean)) return null;
 
-    return (
-      <Box id='exportJobData' className="jobDataContainer">
-        <Box className="jobDescriptionContainer">
-          {
-            exportLinks && expirationTimeUTC && 
-            <>
-              <Typography tag="span" className={`${hasExpired ? 'expired' : 'valid'}`}>
-                { intl.formatMessage({id: 'system-status.export-details.expirationDate'}) + ': ' + formattedExpirationTime}
-              </Typography>
-              { " | " }
-            </>
-          }
-          <Typography tag="bdi" className="jobDescriptionLabel">
-            {jobDescriptionText}
-          </Typography>
-          <Typography tag="bdi" className="jobDescription">
-            {jobDescription.length === NONE
-              ? jobNoDescriptionText
-              : jobDescription}
-          </Typography>
-        </Box>
-
-        {typeof exportLinks !== 'undefined' && jobStatus === Status.Completed && (
-          <Box className="exportJobLinkContainer">
-            {Object.entries(exportLinks).map(([linkType, exportLink]) => {
-              const typeToPresent = linkType.replace('URI', '');
-              return (
-                <Box className='linkContainer' key={`${jobData.id}_${linkType}`}>
-                  <Hyperlink className='jobDataLink' url={exportLink} label={typeToPresent} />
-                  <Copy value = {exportLink} iconStyle = {{ fontSize: `20px` }} key={exportLink}/>
-                </Box>
-              );
-            })}
-          </Box>
-        )}
+  return (
+    <Box id='exportJobData' className="jobDataContainer">
+      <Box className="jobDescriptionContainer">
+        {
+          exportLinks && expirationTimeUTC && 
+          <>
+            <Typography tag="span" className={`${hasExpired ? 'expired' : 'valid'}`}>
+              { intl.formatMessage({id: 'system-status.export-details.expirationDate'}) + ': ' + formattedExpirationTime}
+            </Typography>
+            { " | " }
+          </>
+        }
+        <Typography tag="bdi" className="jobDescriptionLabel">
+          {jobDescriptionText}
+        </Typography>
+        <Typography tag="bdi" className="jobDescription">
+          {jobDescription.length === NONE
+            ? jobNoDescriptionText
+            : jobDescription}
+        </Typography>
       </Box>
-    );
-}
 
-export default JobDetailsExportJobData;
+      {typeof exportLinks !== 'undefined' && jobStatus === Status.Completed && (
+        <Box className="exportJobLinkContainer">
+          {Object.entries(exportLinks).map(([linkType, exportLink]) => {
+            const typeToPresent = linkType.replace('URI', '');
+            return (
+              <Box className='linkContainer' key={`${jobData.id}_${linkType}`}>
+                <Hyperlink className='jobDataLink' url={exportLink} label={typeToPresent} />
+                <Copy value = {exportLink} iconStyle = {{ fontSize: `20px` }} key={exportLink}/>
+              </Box>
+            );
+          })}
+        </Box>
+      )}
+    </Box>
+  );
+};
