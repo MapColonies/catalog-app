059273d78ceca44dd64b4014d7b941c7748a729a
Alex Lebinson <newdarkvirus@gmail.com>
2026-03-31 13:46:14 +0300

    feat: geo worker api (#871)
    * feat: worker as api
    
    * chore: test
    
    * fix: missing label
    
    * fix: worker apis and progress
    
    * chore: prettier
    
    * chore: log
    
    * chore: prettier
    
    * fix: add types rbush
    
    * fix: add rbush 401
    
    * fix: types rbush deps
    
    ---------
    
    Co-authored-by: ellamartirosyan <ellamartirosyan@gmail.com>

---
+++ src/discrete-layer/components/layer-details/raster/worker/useWorkerAPI.ts
@@ -0,0 +1,144 @@
+import { useEffect, useMemo, useState } from 'react';
+import { wrap, Remote, proxy } from 'comlink';
+import type { BBoxObj, WorkerAPI, WorkerMessage } from './worker.types';
+import { FeatureCollection, Geometry } from 'geojson';
+
+export function useWorkerAPI(): {
+  init: {
+    method: () => Promise<void>;
+  };
+  load: {
+    method: (fc: FeatureCollection) => Promise<void>;
+    progress: string;
+  };
+  loadFromShapeFile: {
+    method: (url: string) => Promise<void>;
+    progress: WorkerMessage | null;
+  };
+  updateAreas: {
+    method: () => Promise<void>;
+    progress: WorkerMessage | null;
+  };
+  computeOuterGeometry: {
+    method: () => Promise<Geometry>;
+    progress: WorkerMessage | null;
+  };
+  getFeatureCollection: {
+    method: () => Promise<FeatureCollection>;
+    progress: WorkerMessage | null;
+  };
+  query: {
+    method: (bbox: BBoxObj) => Promise<FeatureCollection>;
+    progress: WorkerMessage | null;
+  };
+} | null {
+  const [workerApi, setWorkerApi] = useState<any>(null);
+  const [progressComputeArea, setProgressComputeArea] = useState<WorkerMessage | null>(null);
+  const [progressLoadShapeFile, setProgressLoadShapeFile] = useState<WorkerMessage | null>(null);
+
+  useEffect(() => {
+    const worker = new Worker(new URL('./feat-collection.worker-api.ts', import.meta.url), {
+      type: 'module',
+    });
+
+    const wrapped: Remote<WorkerAPI> = wrap(worker);
+    setWorkerApi({
+      init: async () => await wrapped.init(),
+      dispose: async () => await wrapped.dispose(),
+      load: async (fc: FeatureCollection) => await wrapped.load(fc),
+      loadFromShapeFile: async (url: string, onProgress?: (p: WorkerMessage | null) => void) =>
+        await wrapped.loadFromShapeFile(url, onProgress),
+      updateAreas: async (onProgress?: (p: WorkerMessage | null) => void) =>
+        await wrapped.updateAreas(onProgress),
+      computeOuterGeometry: async (onProgress?: (p: WorkerMessage | null) => void) =>
+        await wrapped.computeOuterGeometry(onProgress),
+      getFeatureCollection: async (onProgress?: (p: WorkerMessage | null) => void) =>
+        await wrapped.getFeatureCollection(onProgress),
+      query: async (
+        bbox: BBoxObj,
+        onProgress?: (p: WorkerMessage | null) => void
+      ): Promise<FeatureCollection> => await wrapped.query(bbox, onProgress),
+    });
+
+    return () => {
+      wrapped.dispose();
+      worker.terminate();
+    };
+  }, []);
+
+  const api = useMemo(() => {
+    if (!workerApi) return null;
+
+    return {
+      init: {
+        method: async () => {
+          return await workerApi.init();
+        },
+      },
+      load: {
+        method: async (fc: FeatureCollection) => {
+          return await workerApi.load(fc);
+        },
+        progress: '-1',
+      },
+      loadFromShapeFile: {
+        method: async (url: string) => {
+          setProgressLoadShapeFile(null);
+          return await workerApi.loadFromShapeFile(
+            url,
+            proxy((p: WorkerMessage) => {
+              console.log('**** Progress LoadShape: ', p);
+              setProgressLoadShapeFile(p);
+            })
+          );
+        },
+        progress: progressLoadShapeFile,
+      },
+      updateAreas: {
+        method: async () => {
+          setProgressComputeArea(null);
+          return await workerApi.updateAreas(
+            proxy((p: WorkerMessage) => {
+              console.log('**** Progress Area: ', p);
+              setProgressComputeArea(p);
+            })
+          );
+        },
+        progress: progressComputeArea,
+      },
+      computeOuterGeometry: {
+        method: async () => {
+          return await workerApi.computeOuterGeometry(
+            proxy((p: WorkerMessage) => {
+              console.log('**** Progress Outer Geometry: ', p);
+            })
+          );
+        },
+        progress: null,
+      },
+      getFeatureCollection: {
+        method: async (): Promise<FeatureCollection> => {
+          return await workerApi.getFeatureCollection(
+            proxy((p: WorkerMessage) => {
+              console.log('**** GET FEATURECOLLECTION: ', p);
+            })
+          );
+        },
+        progress: null,
+      },
+      query: {
+        method: async (bbox: BBoxObj): Promise<FeatureCollection> => {
+          return await workerApi.query(
+            bbox,
+            proxy((p: WorkerMessage) => {
+              console.log('**** QUERY by BBOX: ', p);
+            })
+          );
+        },
+        progress: null,
+      },
+    };
+  }, [workerApi, progressComputeArea, progressLoadShapeFile]);
+
+  return api;
+}
---
+++ src/discrete-layer/components/layer-details/raster/worker/worker.types.ts
@@ -0,0 +1,32 @@
+import { FeatureCollection, Geometry } from 'geojson';
+
+export type BBoxObj = {
+  minX: number;
+  minY: number;
+  maxX: number;
+  maxY: number;
+};
+export type IndexedItem = BBoxObj & { i: number };
+
+export interface WorkerAPI {
+  init(): Promise<void>;
+  dispose(): void;
+  load(fc: FeatureCollection): Promise<void>;
+  loadFromShapeFile(url: string, onProgress?: (p: WorkerMessage | null) => void): Promise<void>;
+  updateAreas(onProgress?: (p: WorkerMessage | null) => void): void;
+  computeOuterGeometry(onProgress?: (p: WorkerMessage | null) => void): Geometry;
+  getFeatureCollection(onProgress?: (p: WorkerMessage | null) => void): FeatureCollection;
+  query(bbox: BBoxObj, onProgress?: (p: WorkerMessage | null) => void): FeatureCollection;
+}
+
+export interface WorkerMessage {
+  process:
+    | 'Download'
+    | 'Parsing'
+    | 'Cache'
+    | 'UpdateAreas'
+    | 'ComputeOuterGeometry'
+    | 'GetFeatureCollection';
+  type: 'Progress' | 'Done' | 'Error';
+  message: string;
+}
