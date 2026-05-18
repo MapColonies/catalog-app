8e591255e216609772982e24fb60f7a176ac90fb
Ella Martirosyan <34953077+EllaMartirosyan@users.noreply.github.com>
2026-05-18 19:49:08 +0300

    fix: remove default dot at public url (#960)
    * fix: remove default dot at public url
    
    * fix: fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,10 @@ echo "Done with confd"
 
 PUBLIC_URL_NORMALIZED="${CONFIGURATION_PUBLIC_URL:-}"
 
+if [ "$PUBLIC_URL_NORMALIZED" = "/" ]; then
+  PUBLIC_URL_NORMALIZED=""
+fi
+
 ESCAPED_PUBLIC_URL="$(printf '%s' "$PUBLIC_URL_NORMALIZED" | sed 's/[^a-zA-Z0-9.]/\\&/g')"
 
 echo "Running SED command -->" 's/{PUBLIC_URL_PLACEHOLDER}/'"$ESCAPED_PUBLIC_URL"'/g'
