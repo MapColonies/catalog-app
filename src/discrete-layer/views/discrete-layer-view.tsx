a83a8120e15ed9dda197ac18aef9d1108537e912
ellamartirosyan <ellamartirosyan@gmail.com>
2026-06-30 20:54:21 +0300

    fix: cesium upgrade

--- src/discrete-layer/views/discrete-layer-view.tsx
+++ src/discrete-layer/views/discrete-layer-view.tsx
@@ -32,6 +32,7 @@ import {
   IContextMenuData,
   IDrawing,
   IDrawingEvent,
+  ILayerManagerMetaMapping,
 } from '@map-colonies/react-components';
 import { GeocoderOptions } from '@map-colonies/react-components/dist/cesium-map/geocoder/geocoder-panel';
 import { IMapLegend } from '@map-colonies/react-components/dist/cesium-map/legend';
@@ -996,6 +997,20 @@ const DiscreteLayerView: React.FC = observer(() => {
     return actionsMenuDimensions as MenuDimensions;
   }, [activeTabView, actionsMenuDimensions]);
 
+  const layerManagerMetaMapping = useMemo<ILayerManagerMetaMapping>(() => {
+    return {
+      layer: {
+        id: 'id',
+        name: 'layerRecord.productName',
+        footprint: 'layerRecord.footprint',
+      },
+      dataLayer: {
+        name: 'layerRecord.featureStructure.aliasLayerName',
+        fields: 'layerRecord.featureStructure.fields',
+      },
+    };
+  }, []);
+
   const site = useMemo(() => currentSite(), []);
 
   const triggerCallbackFunc = (data: Feature, options: GeocoderOptions, i: number) => {
@@ -1333,10 +1348,9 @@ const DiscreteLayerView: React.FC = observer(() => {
               center={CONFIG.MAP.CENTER}
               zoom={CONFIG.MAP.ZOOM}
               sceneMode={CesiumSceneMode.SCENE2D}
-              imageryProvider={false}
               locale={mapSettingsLocale}
               baseMaps={store.discreteLayersStore.baseMaps}
-              layerManagerFootprintMetaFieldPath={'layerRecord.footprint'}
+              layerManagerMetaMapping={layerManagerMetaMapping}
               // @ts-ignore
               imageryContextMenu={<ContextMenuByTab />}
               imageryContextMenuSize={contextMenuSizeByTab}
