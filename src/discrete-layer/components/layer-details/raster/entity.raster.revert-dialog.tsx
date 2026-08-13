19c1074b610b1972649f33459718e41adbcf44a4
ellamartirosyan <ellamartirosyan@gmail.com>
2026-08-13 14:57:07 +0300

    chore: clean

--- src/discrete-layer/components/layer-details/raster/entity.raster.revert-dialog.tsx
+++ src/discrete-layer/components/layer-details/raster/entity.raster.revert-dialog.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { observer } from 'mobx-react';
 import { Feature, Geometry } from 'geojson';
 import { Fill, Stroke, Style } from 'ol/style';
