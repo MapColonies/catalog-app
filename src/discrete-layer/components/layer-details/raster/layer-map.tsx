052b355d590bd48925f940f548d78f4313a39c6f
Eliav Maimon <liavma3@gmail.com>
2026-08-26 14:51:07 +0300

    fix: memoize queryExecutor and its dependencies

--- src/discrete-layer/components/layer-details/raster/layer-map.tsx
+++ src/discrete-layer/components/layer-details/raster/layer-map.tsx
@@ -152,26 +152,29 @@ export const OlLayerMap: React.FC<LayerMapProps> = ({
     [intl]
   );
 
-  const queryExecutor = async (bbox: BBox, startIndex: number): Promise<IQueryExecutorResponse> => {
-    const result = await store.queryGetPolygonPartsFeature({
-      data: {
-        feature: bboxPolygon(bbox) as GeojsonFeatureInput,
-        typeName: getWFSFeatureTypeName(layerRecord as LayerRasterRecordModelType, ENUMS),
-        count: CONFIG.POLYGON_PARTS.MAX.WFS_FEATURES,
-        startIndex,
-      },
-    });
-    const fetchedFeatures = get(result, 'getPolygonPartsFeature.features', []);
-    const features = (Array.isArray(fetchedFeatures) ? fetchedFeatures : []).map((feature) => ({
-      ...feature,
-      properties: {
-        ...(feature?.properties ?? {}),
-        _featureType: FeatureType.EXISTING_PP,
-        _featureTitle: getText(feature, 4, FEATURE_LABEL_CONFIG.polygons, ZOOM_LEVELS_TABLE),
-      },
-    }));
-    return { features, pageSize: CONFIG.POLYGON_PARTS.MAX.WFS_FEATURES };
-  };
+  const queryExecutor = useCallback(
+    async (bbox: BBox, startIndex: number): Promise<IQueryExecutorResponse> => {
+      const result = await store.queryGetPolygonPartsFeature({
+        data: {
+          feature: bboxPolygon(bbox) as GeojsonFeatureInput,
+          typeName: getWFSFeatureTypeName(layerRecord as LayerRasterRecordModelType, ENUMS),
+          count: CONFIG.POLYGON_PARTS.MAX.WFS_FEATURES,
+          startIndex,
+        },
+      });
+      const fetchedFeatures = get(result, 'getPolygonPartsFeature.features', []);
+      const features = (Array.isArray(fetchedFeatures) ? fetchedFeatures : []).map((feature) => ({
+        ...feature,
+        properties: {
+          ...(feature?.properties ?? {}),
+          _featureType: FeatureType.EXISTING_PP,
+          _featureTitle: getText(feature, 4, FEATURE_LABEL_CONFIG.polygons, ZOOM_LEVELS_TABLE),
+        },
+      }));
+      return { features, pageSize: CONFIG.POLYGON_PARTS.MAX.WFS_FEATURES };
+    },
+    [store, layerRecord, ENUMS, ZOOM_LEVELS_TABLE]
+  );
 
   const layerCapability = store.discreteLayersStore.getLayerCapability(layerRecord);
 
